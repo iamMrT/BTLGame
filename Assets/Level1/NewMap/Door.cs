ada7aee87b25f8aaaadf49089d649cd2f9fff44b
VanTinh <tinhngovan410@gmail.com>
2022-12-14 23:56:08 +0700

    Add load sence level2 fixbug

--- Assets/Level1/NewMap/Door.cs
+++ Assets/Level1/NewMap/Door.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private Transform previousRoom;
-    [SerializeField] private Transform nextRoom;
-    [SerializeField] private CameraController cam;
-    
+    // [SerializeField] private Transform previousRoom;
+    // [SerializeField] private Transform nextRoom;
+    // [SerializeField] private CameraController cam;
+         model m_model;
+
 
      private void OnTriggerEnter2D(Collider2D collision) {
         // camera theo room
         if(collision.tag =="Player"){
-            if(collision.transform.position.x < transform.position.x) 
-             cam.MoveToNewRoom(nextRoom);
-             else
-             cam.MoveToNewRoom(previousRoom);
+              if(m_model.GetScore() >0)   SceneManager.LoadScene("Level3");
+               else  SceneManager.LoadScene("Level2");
+         
+            // if(collision.transform.position.x < transform.position.x) 
+            //  cam.MoveToNewRoom(nextRoom);
+            //  else
+            //  cam.MoveToNewRoom(previousRoom);
+
         }
 
  
       }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+             m_model = FindObjectOfType<model>();
+
     }
 
     // Update is called once per frame
