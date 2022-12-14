b1d44860967c2fc8257e4cfb01b3a0dae34eae15
tranducanh02 <tranducanh21101@gmail.com>
2022-12-14 12:55:29 +0700

    Duc Anh them am thanh

--- Assets/Level1/Health/HealthCollectible.cs
+++ Assets/Level1/Health/HealthCollectible.cs
@@ -6,7 +6,6 @@ public class HealthCollectible : MonoBehaviour
 {
    
     [SerializeField] private float healthValue;
-
    private void OnTriggerEnter2D(Collider2D collision) {
      if(collision.tag == "Player"){
         collision.GetComponent<Health>().AddHealth(healthValue);
